5d7f1611063a43cbfbb664688d720e7e7ef5f089
nerdelt <nerdelt@gmail.com>
2018-10-11 13:01:24 -0500

    Update

--- StudentNames/StudentNames/Program.cs
+++ StudentNames/StudentNames/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentNames
 {
@@ -6,18 +9,47 @@ namespace StudentNames
     {
         static void Main(string[] args)
         {
-
             //creates new student object
             Student student = new Student();
 
-            Console.Write("To find out the names of the students who have more than 6 quiz scores, " +
-                "please press ENTER.\n");
+            Console.Write("The following students have more than 6 quiz scores.\n");
 
-            Console.ReadLine();
+        
+            using (StreamReader sr = new StreamReader(@"C:\Users\nerdelt\Source\Repos\CG-11-1\StudentNames\StudentNames\studentdata.txt"))
+            {
+                string line;
 
-            //calls method to get students name who has more than 6 scores and prints to console
-            Console.WriteLine(student.GetStudentName());
-            Console.ReadLine();
+                //creates a list for all the split strings from Students
+                List<string> splitList = new List<string>();
+
+                {
+                    //while loop to add each line as an element in  list Students
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        student.Students.Add(line);
+                    }
+                }
+
+                foreach (string s in student.Students)
+                {
+                    //splits each string into smaller strings and adds them to new list
+                    splitList = s.Split(' ').ToList();
+
+                    // assigns name
+                    student.Name = splitList[0];
+
+                    int scores = splitList.Count;
+                    if (scores > 7)
+                    {
+                        Console.WriteLine($"{student.Name}");
+                    }
+                }
+
+                    Console.ReadLine();
+
+                }
+            }
         }
     }
-}
+
+
--- StudentNames/StudentNames/Student.cs
+++ StudentNames/StudentNames/Student.cs
@@ -4,40 +4,21 @@ using System.Text;
 using System.IO;
 using System.Linq;
 
+
 namespace StudentNames
 {
     //creates student class
-    class Student
+    public class Student
     {
         // creates string array for txt file to go into
-        public string[] StudentData { get; set; }
+        public List<string> Students = new List<string>();
 
         //creates string property to be returned 
-        public string Name { get; set; }
-
-        //creates method for getting students name if they have more than 6 quiz scores
-        public string GetStudentName()
-        {
-            //reads txt file
-            StudentData = File.ReadAllLines(@"C:\Users\nerdelt\Source\Repos\CG-11-1\StudentNames\StudentNames\studentdata.txt");
-
-            //creates a loop to spilt each line into array
-            //then counts items in array and only returns the name of those that have more than 6 scores
-            foreach (string s in StudentData)
-            {
-                string[] score = s.Split(' ');
-                int l = score.Length;
-                if (l > 7)
-                {           
-                    Name = score[0];
-                }
-            }
-                return Name;
-            
+        public string Name { get; set; }        
 
         }
     }
-}
+
 
 
 
