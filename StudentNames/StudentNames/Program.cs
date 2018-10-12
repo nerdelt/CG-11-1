c8cdafb9d93b28850e937f3bc17c2641f7806592
nerdelt <nerdelt@gmail.com>
2018-10-12 06:35:08 -0500

    Changed List to main

--- StudentNames/StudentNames/Program.cs
+++ StudentNames/StudentNames/Program.cs
@@ -14,6 +14,7 @@ namespace StudentNames
 
             Console.Write("The following students have more than 6 quiz scores.\n");
 
+            List<string> Students = new List<string>();
         
             using (StreamReader sr = new StreamReader(@"C:\Users\nerdelt\Source\Repos\CG-11-1\StudentNames\StudentNames\studentdata.txt"))
             {
@@ -26,11 +27,11 @@ namespace StudentNames
                     //while loop to add each line as an element in  list Students
                     while ((line = sr.ReadLine()) != null)
                     {
-                        student.Students.Add(line);
+                        Students.Add(line);
                     }
                 }
 
-                foreach (string s in student.Students)
+                foreach (string s in Students)
                 {
                     //splits each string into smaller strings and adds them to new list
                     splitList = s.Split(' ').ToList();
--- StudentNames/StudentNames/Student.cs
+++ StudentNames/StudentNames/Student.cs
@@ -10,9 +10,6 @@ namespace StudentNames
     //creates student class
     public class Student
     {
-        // creates string array for txt file to go into
-        public List<string> Students = new List<string>();
-
         //creates string property to be returned 
         public string Name { get; set; }        
 
